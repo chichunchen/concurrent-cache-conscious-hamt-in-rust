4760c2beee208dcc375606490081db247eef95bb
chichunchen <chichunchen844@gmail.com>
2018-03-28 15:40:44 -0400

    Init cargo

---
+++ main.rs
@@ -0,0 +1,25 @@
+//struct TrieArray<'a> {
+//	array: Vec<&'a Node<'a>>
+//}
+//
+//enum Node<'a> {
+//	TrieArray{Vec<&'a Node<'a>>},
+//	Value(String),
+//	Null
+//}
+
+fn main() {
+//	let leaf_1 = Node::Null;
+//	let leaf_2 = Node::Value(String::from("haha"));
+//	let mut root_vec = Vec::new();
+//	root_vec.push(&leaf_1);
+//	root_vec.push(&leaf_2);
+//	let root = Node::Array(root_vec);
+//
+//	// pattern matching
+// 	match root {
+//		Node::TrieArray { vec } => println!("todo"),
+//		Node::Value ( s ) => println!("{}", s),
+//		Node::Null => println!("notring"),
+// 	}
+}
