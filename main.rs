0e828a274c3069c879175fabd06f078a693ac7cf
chichunchen <chichunchen844@gmail.com>
2018-03-29 11:30:52 -0400

    Add get and update all tests

--- main.rs
+++ main.rs
@@ -17,10 +17,26 @@ pub enum IndexStatus {
     NoMatch,
 }
 
+const KEY_LEN: usize = 16;
+const KEY_GROUP: usize = 4;
+
+
+// index is the sum of binary in a group
+fn compute_index(key: &[u8]) -> usize {
+    let mut id = 0;
+    let length = if key.len() > KEY_GROUP { KEY_GROUP } else { key.len() };
+    for i in 0..length {
+        let temp = key[i] as usize - '0' as usize;
+        id += temp << i;
+    }
+
+    return id as usize;
+}
+
 impl<T: TrieData> Trie<T> {
     pub fn new() -> Self {
-        let mut children = Vec::with_capacity(16);
-        for _ in 0..16 {
+        let mut children = Vec::with_capacity(KEY_LEN);
+        for _ in 0..KEY_LEN {
             children.push(None);
         }
         Trie { data: None, depth: 0, children: children }
@@ -30,43 +46,38 @@ impl<T: TrieData> Trie<T> {
         self.depth
     }
 
-    pub fn add_value(&mut self, value: T, key: &[u8]) -> u32 {
+    // insert a generic value by a key, and the key should be in binary format
+    pub fn insert(&mut self, value: T, key: &[u8]) -> u32 {
         if key.len() == 0 {
             self.data = Some(value);
             return 1;
         } else {
-            // TODO index is the sum of binary in a group
-            //let index = key[0] as usize - 'a' as usize;
-            let mut id = 0;
-            let length = key.len();
-            if length < 4 {
-                for i in 0..length {
-                    let temp = key[i] as usize - '0' as usize;
-                    println!("test tt {}", temp);
-                    id += (temp << i);
-                }
-            } else {
-                for i in 0..4 {
-                    let temp = key[i] as usize - '0' as usize;
-                    println!("test tt {}", temp);
-                    id += (temp << i);
-                }
-            }
-
-            let index = id as usize;
-            println!("test index {}", index);
+            let index = compute_index(key);
 
             // if the array has not been created, then create one
             if self.children[index].is_none() {
                 println!("create subtree");
                 self.children[index] = Some(Box::new(Trie::new()));
             }
-            let value= match length {
-                n if n >= 4 => self.children[index].as_mut().map(|ref mut a| a.add_value(value, &key[4..])).unwrap_or(0),
-                _          => 9999,
+            let value = match key.len() {
+                n if n >= KEY_GROUP => self.children[index].as_mut().map(|ref mut a| a.insert(value, &key[KEY_GROUP..])).unwrap_or(0),
+                _ => 9999,
             };
             self.depth += value;
             return value;
+        }
+    }
+
+    // get value from key
+    pub fn get(&self, key: &[u8]) -> Option<T> {
+        let result = self.get_sub_trie(key);
+
+        match result {
+            Some(trie) => match trie.data {
+                Some(data) => return Some(data),
+                _ => return None,
+            }
+            _ => return None,
         }
     }
 
@@ -74,30 +85,16 @@ impl<T: TrieData> Trie<T> {
         if key.len() == 0 {
             self.data.map(|_| IndexStatus::FullMatch).unwrap_or(IndexStatus::StartingMatch)
         } else {
-            let index = key[0] as usize - '0' as usize;
-            self.children[index].as_ref().map(|ref a| a.index_base(&key[1..])).unwrap_or(IndexStatus::NoMatch)
+            let index = compute_index(key);
+            self.children[index].as_ref().map(|ref a| a.index_base(&key[KEY_GROUP..])).unwrap_or(IndexStatus::NoMatch)
         }
     }
 
     pub fn get_sub_trie<'a>(&'a self, key: &[u8]) -> Option<&'a Trie<T>> {
-        let mut id = 0;
-        let length = key.len();
-        if length < 4 {
-            for i in 0..length {
-                let temp = key[i] as usize - '0' as usize;
-                println!("test tt {}", temp);
-                id += (temp << i);
-            }
-        } else {
-            for i in 0..4 {
-                let temp = key[i] as usize - '0' as usize;
-                id += (temp << i);
-            }
-        }
-        let index = id as usize;
-        match length {
-            n if n >= 4 => self.children[index].as_ref().and_then(|ref a| a.get_sub_trie(&key[4..])),
-            _          => Some(&self),
+        let index = compute_index(key);
+        match key.len() {
+            n if n >= KEY_GROUP => self.children[index].as_ref().and_then(|ref a| a.get_sub_trie(&key[KEY_GROUP..])),
+            _ => Some(&self),
         }
     }
 }
@@ -109,56 +106,59 @@ fn test_new_trie() {
 }
 
 #[test]
-fn test_add_value() {
+fn test_insert() {
     let mut base = Trie::new();
 
-    base.add_value((), &"1111111111111111".to_owned().into_bytes());
-    base.add_value((), &"1111101011111111".to_owned().into_bytes());
-    base.add_value((), &"1111111111111011".to_owned().into_bytes());
-    base.add_value((), &"1111111111111110".to_owned().into_bytes());
-    base.add_value((), &"1111111111111101".to_owned().into_bytes());
-    base.add_value((), &"1111111111111110".to_owned().into_bytes());
+    base.insert((), &"1111111111111111".to_owned().into_bytes());
+    base.insert((), &"1111101011111111".to_owned().into_bytes());
+    base.insert((), &"1111111111111011".to_owned().into_bytes());
+    base.insert((), &"1111111111111110".to_owned().into_bytes());
+    base.insert((), &"1111111111111101".to_owned().into_bytes());
+    base.insert((), &"1111111111111110".to_owned().into_bytes());
 }
 
 #[test]
 fn test_index_base() {
-//    let mut base = Trie::new();
-//
-//    base.add_value((), &"start".to_owned().into_bytes());
-//    base.add_value((), &"starting".to_owned().into_bytes());
-//    base.add_value((), &"red".to_owned().into_bytes());
-//    base.add_value((), &"zzzzz".to_owned().into_bytes());
-//    base.add_value((), &"aaaaaa".to_owned().into_bytes());
-//    base.add_value((), &"redz".to_owned().into_bytes());
-//
-//    assert_eq!(base.index_base(&"start".to_owned().into_bytes()), IndexStatus::FullMatch);
-//    assert_eq!(base.index_base(&"sta".to_owned().into_bytes()), IndexStatus::StartingMatch);
-//    assert_eq!(base.index_base(&"started".to_owned().into_bytes()), IndexStatus::NoMatch);
-//    assert_eq!(base.index_base(&"ggg".to_owned().into_bytes()), IndexStatus::NoMatch);
+    let mut base = Trie::new();
+
+    base.insert((), &"1111111111111111".to_owned().into_bytes());
+    base.insert((), &"0110111111111111".to_owned().into_bytes());
+
+    assert_eq!(base.index_base(&"1111111111111111".to_owned().into_bytes()), IndexStatus::FullMatch);
+    assert_eq!(base.index_base(&"1110111111111110".to_owned().into_bytes()), IndexStatus::NoMatch);
 }
-	
+
+#[test]
+fn test_get() {
+    let mut base = Trie::new();
+
+    base.insert("abc", &"1111111111111111".to_owned().into_bytes());
+    base.insert("cde", &"0110111111111111".to_owned().into_bytes());
+
+    let a = base.get(&"1111111111111111".to_owned().into_bytes());
+    let ab = base.get(&"0110111111111111".to_owned().into_bytes());
+
+    match a {
+        Some(d) => assert_eq!(d, "abc"),
+        _ => println!("find none"),
+    }
+
+    match ab {
+        Some(d) => assert_ne!(d, "add"),
+        _ => println!("find none"),
+    }
+}
+
 fn main() {
     let mut base = Trie::new();
     let a = 1;
-    base.add_value(a, &"1111111111111111".to_owned().into_bytes());
-    println!("end1");
 
-    base.add_value(2, &"1111111111111101".to_owned().into_bytes());
+    base.insert(a, &"1111111111111111".to_owned().into_bytes());
+    base.insert(2, &"1111111111111101".to_owned().into_bytes());
 
-    let result = base.get_sub_trie(&"1111111111111101".to_owned().into_bytes());
-    println!("end2");
-
-    match result {
-        Some(trie) => match trie.data {
-            Some(fuck) => println!("{}", fuck),
-            _ => println!("not found 2"),
-        }
-        _ => println!("not found 1"),
+    let op = base.get(&"1111111111111101".to_owned().into_bytes());
+    match op {
+        Some(d) => println!("{}", d),
+        _ => println!("find none"),
     }
-
-//
-//    let test = &"start".to_owned().into_bytes();
-//    for x in test {
-//        println!("{}", x - 97); // x: i32
-//    }
 }
